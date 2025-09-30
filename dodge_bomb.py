526c07e4e5d38c73722625bfdbafd1d4e0c99693
c0a24243 <c0a2424362@edu.teu.ac.jp>
2025-09-30 17:43:59 +0900

    空行の数を規定通りにしてください

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -60,7 +60,6 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     return bb_imgs, bb_acclee  # 爆弾画像リスト、爆弾Rectリストを返す
 
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))

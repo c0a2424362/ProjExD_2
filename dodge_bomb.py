b5c719f186e61d6ce86870a501600bf860d550e2
c0a24243 <c0a2424362@edu.teu.ac.jp>
2025-09-30 17:21:04 +0900

    演習課題2途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -27,6 +27,39 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def gemeover(screen:pg.Surface) -> None:  # ゲームオーバー
+    screen = pg.display.set_mode((WIDTH, HEIGHT))  # 画面設定
+    gemeover_bg = pg.Surface((WIDTH, HEIGHT))  # ゲームオーバー背景
+    gemeover_bg.fill((0, 0, 0))  # 黒で塗りつぶし
+    gemeover_bg.set_alpha(200)  # 透明度設定
+    font = pg.font.Font(None, 150)
+    txt = font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(txt, [300, 200])
+    gemeover_img = pg.image.load("fig/8.png")
+    screen.blit(gemeover_img, [0, 0])  # ゲームオーバー背景描画
+    screen.blit(gemeover_bg, [0, 0])  # ゲームオーバー文字描画
+    pg.display.update()
+    time.sleep(5)  # 5秒間表示
+    pg.quit()
+    sys.exit()
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = []  # 爆弾画像リスト
+    bb_acclee = []  # 爆弾加速度リスト
+    for r in range(1, 11):  # 10個の爆弾
+        bb_img = pg.Surface((20*r, 20*r))  # 爆弾用の空Surface
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)  # 赤い爆弾円
+        bb_imgs.append(bb_img)  # 爆弾画像リストに追加
+    bb_acclee = [a for a in range(1, 11)]  # 爆弾加速度リストに追加
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空Surface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い爆弾円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅の黒い部分を透過 
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    return bb_imgs, bb_acclee  # 爆弾画像リスト、爆弾Rectリストを返す
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -42,6 +75,9 @@ def main():
     bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
     bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
     vx, vy = +5, +5  # 爆弾の速度
+    init_bb_imgs, bb_rcts = init_bb_imgs()  # 爆弾画像リスト、爆弾Rectリスト
+    bb_imgs = init_bb_imgs  # 爆弾画像リスト
+    avx, avy = vx, vy  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -50,35 +86,8 @@ def main():
                 return
         screen.blit(bg_img, [0, 0])
         if kk_rct.colliderect(bb_rct):
-            gemeover()  # geme over
-
-        def gemeover() -> None:
-            go_sfc = pg.Surface((WIDTH, HEIGHT))  # ゲームオーバー用の空Surface
-            go_sfc.set_alpha(200)  # 透明度設定
-            go_sfc.fill((0, 0, 0))  # 黒で塗りつぶし
-            screen.blit(go_sfc, [0, 0])  # 画面に貼り付け   
-            font = pg.font.Font(None, 150)  # フォントオブジェクトを作成
-            txt = font.render("GAME OVER", True, (255, 255, 255))  # 白のGAME OVERテキスト
-            screen.blit(txt, [300, 200])  # テキストを画面に貼り付け[位置]
-            go_img = pg.image.load("fig/8.png")  # ゲームオーバー画像の読み込み
-            go_rct = go_img.get_rect() # ゲームオーバー画像のRect
-            go_rct.center = 300, 400  # ゲームオーバー画像の位置
-            screen.blit(go_img, go_rct)  # ゲームオーバー画像の貼り付け
-            pg.display.update()
-            time.sleep(5)  # 5秒間表示
-            pg.quit()
-        #def gemeover(screen: pg.Sruface) -> None:
-            #go_img = pg.Surface((WIDTH, HEIGHT))  # ゲームオーバー用の空Surface
-            #go_img.get_alpha()  # 透明度設定
-            #font = pg.font.Font(None, 150)  # フォントオブジェクトを作成
-            #txt = font.render("geme over", True, (255, 255, 255))  # 白のgeme overテキスト
-            #screen.bilt(txt, [300, 200])  # テキストを画面に貼り付け[位置]
-            #go_img = pg.image.load("fig/6.png")  # ゲームオーバー画像の読み込み
-            #go_rct = go_img.get_rect()  # ゲームオーバー画像のRect
-            #screen.blit(go_img,go_rct)  # ゲームオーバー画像の貼り付け
-            #pg.display.update()
-            #time.sleep(5)  # 5秒間表示
-            return
+            gemeover(screen)  # geme over
+        
         
 
 
@@ -102,7 +111,7 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rect.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)  # 爆弾移動
+        bb_rct.move_ip(avx, avy)  # 爆弾移動
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
